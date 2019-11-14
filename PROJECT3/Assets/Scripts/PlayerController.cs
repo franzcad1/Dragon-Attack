39e06d2ccd6c8e40cdd1279d43e7d081133c5616
Franz <fcadien1@my.centennialcollege.ca>
2019-11-13 21:52:17 -0500

    Added Sol, Movement, and Spikes kill player

---
+++ PROJECT3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    private Rigidbody2D rBody;
+
+    private bool isGrounded;
+    public Transform feetPos;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horiz = Input.GetAxis("Horizontal");
+        
+        rBody.velocity = new Vector2(horiz * speed, rBody.velocity.y);
+    }
+
+    private void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+
+        if(isGrounded == true && (Input.GetKeyDown(KeyCode.Space) || (Input.GetKeyDown(KeyCode.W)))){
+            rBody.velocity = Vector2.up * jumpForce;
+        }
+    }
+}
