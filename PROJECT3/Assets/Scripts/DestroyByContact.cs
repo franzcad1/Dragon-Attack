b148207fbdcbd324742e9d088b20deabc69ba15d
Franz <fcadien1@my.centennialcollege.ca>
2019-11-20 18:47:45 -0500

    Added death animation, fixed playtest problems, and added restart

--- PROJECT3/Assets/Scripts/DestroyByContact.cs
+++ PROJECT3/Assets/Scripts/DestroyByContact.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour
 {
+    public GameObject explosion;
     // Start is called before the first frame update
+    private GameController gameControllerScript;
     void Start()
-    {
-        
-    }
+        {
+            GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+
+            if (gameControllerObject != null)
+            {
+                // I got the game controller object!
+                gameControllerScript = gameControllerObject.GetComponent<GameController>();
+
+                if (gameControllerScript == null)
+                {
+                    // There is no GameController script on my game controller object
+                    Debug.Log("Cannot find GameController script on GameController object");
+                }
+            }
+        }
+    
 
     // Update is called once per frame
     void Update()
@@ -17,9 +32,18 @@ public class DestroyByContact : MonoBehaviour
     }
     void OnCollisionEnter2D(Collision2D other)
     {
-        
+        Instantiate(explosion, other.transform.position, other.transform.rotation);
 
-        Destroy(other.gameObject); 
+        if (other.gameObject.CompareTag("Player"))
+        {
+            // Collided with the player object
+            gameControllerScript.GameOver();
+        }
+        Destroy(other.gameObject);  
+
+     
         
+       
     }
+  
 }
---
+++ PROJECT3/Assets/Scripts/GameController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    private bool gameOver, restart;
+
+    [Header("UI Settings")]
+    public Text scoreText;
+    public Text gameOverText;
+    public Text restartText;
+
+    void Start()
+    {
+        gameOver = restart = false;
+        
+
+    }
+    void Update()
+    {
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            // QUIT THE GAME
+            Application.Quit();
+        }
+
+        if (gameOver)
+        {
+            restartText.gameObject.SetActive(true);
+            restart = true;
+        }
+
+    }
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        gameOver = true;
+    }
+
+    
+}
